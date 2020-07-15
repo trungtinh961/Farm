be36b0c9f67c5ea25318279d8298f1ba502b8fee
trungtinh961 <39235800+trungtinh961@users.noreply.github.com>
2020-07-15 21:53:12 +0700

    history

---
+++ app/src/main/java/com/example/farm/Alert.java
@@ -0,0 +1,30 @@
+package com.example.farm;
+
+/**
+ * Created by Trung Tinh on 7/15/2020.
+ */
+public class Alert {
+    public int temparature;
+    public String time;
+
+    public Alert(int temparature, String time) {
+        this.temparature = temparature;
+        this.time = time;
+    }
+
+    public int getTemparature() {
+        return temparature;
+    }
+
+    public void setTemparature(int temparature) {
+        this.temparature = temparature;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
--- app/src/main/java/com/example/farm/ui/HistoryFragment.java
+++ app/src/main/java/com/example/farm/ui/HistoryFragment.java
@@ -8,14 +8,22 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.farm.Alert;
 import com.example.farm.R;
-import com.github.lzyzsd.circleprogress.ArcProgress;
+import com.example.farm.alertAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by Trung Tinh on 7/15/2020.
  */
 public class HistoryFragment extends Fragment {
+
+    public View view;
 
     @Nullable
     @Override
@@ -23,9 +31,26 @@ public class HistoryFragment extends Fragment {
 
         View view =  inflater.inflate(R.layout.fragment_history, container, false);
 
-        
+        initView();
 
         return view;
 
+    }
+
+    private void initView() {
+        final RecyclerView rvHistory = (RecyclerView) view.findViewById(R.id.recyclerviewHistory);
+        rvHistory.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false);
+        rvHistory.setLayoutManager(layoutManager);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getActivity(),layoutManager.getOrientation());
+        rvHistory.addItemDecoration(dividerItemDecoration);
+
+        final ArrayList<Alert> alertArrayList = new ArrayList<>();
+        alertArrayList.add(new Alert(20,"11/06/2020"));
+        alertArrayList.add(new Alert(20,"11/06/2020"));
+        alertArrayList.add(new Alert(20,"11/06/2020"));
+        alertArrayList.add(new Alert(20,"11/06/2020"));
+        alertAdapter alertAdapter  = new alertAdapter(getContext(),R.layout.item_alert,alertArrayList);
+        rvHistory.setAdapter(alertAdapter);
     }
 }
